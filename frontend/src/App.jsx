8d734d4bf6054de4962c3eceeb5ab9c937e78ffe
jaideep2121 <sachdevajaideep3@gmail.com>
2024-11-04 19:53:53 +0530

    url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -31,7 +31,7 @@ function App() {
 
   useEffect(() => {
     if (user) {
-      const socketio = io('http://localhost:4000', {
+      const socketio = io('https://logo-media-i3uh.onrender.com', {
         query: {
           userId: user?._id
         },
