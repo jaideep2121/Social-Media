6dc64b29c68ff1f72c42f6bdd74f27b53fa7c25e
jaideep2121 <sachdevajaideep3@gmail.com>
2024-11-04 17:49:39 +0530

    ready for deployment

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(urlencoded({ extended: true }));
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: "https://logo-media-i3uh.onrender.com",
     credentials: true
 }
 app.use(cors(corsOptions));
