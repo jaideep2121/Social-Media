ee02f86542351ebfe8ff1dcc0bd08dd234e6b0ef
jaideep2121 <sachdevajaideep3@gmail.com>
2024-11-05 13:00:26 +0530

    url

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URL);
+        await mongoose.connect("mongodb://127.0.0.1/Social-Media");
       
         console.log('mongodb connected successfully.');
     } catch (error) {
