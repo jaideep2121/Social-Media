028979f569a4016b4ad06070becca69ab478083a
jaideep2121 <sachdevajaideep3@gmail.com>
2024-11-05 16:07:35 +0530

    url

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(urlencoded({ extended: true }));
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: "https://logo-social-media.onrender.com",
     credentials: true
 }
 app.use(cors(corsOptions));
